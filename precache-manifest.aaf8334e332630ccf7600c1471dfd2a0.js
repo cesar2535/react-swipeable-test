d8086802bb952c134c044962900d30ac95935f4f
Cesar Chen <cesar6134@gmail.com>
2019-03-23 00:49:45 +0800

    Updates

---
+++ precache-manifest.aaf8334e332630ccf7600c1471dfd2a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9652aa1b51cc86c8dd15",
+    "url": "/static/css/main.e54e83cb.chunk.css"
+  },
+  {
+    "revision": "9652aa1b51cc86c8dd15",
+    "url": "/static/js/main.809592c9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2a1a90d340342c1ce19d",
+    "url": "/static/js/2.bd6f1ee0.chunk.js"
+  },
+  {
+    "revision": "650b2d02a281d692abfc4154fe68c5b0",
+    "url": "/index.html"
+  }
+];
